6ea02471bb4e7e39042991f525746cddc3b54309
vh <vh@smena.space>
2020-10-06 16:22:29 +0500

    Remove useless stuff

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Pypi password for $PYPI_USERNAME: "
-read -s password
-
-docker build -f docker/Dockerfile.manylinux2014 --build-arg PYPI_USERNAME --build-arg PYPI_PASSWORD=$password .
-
